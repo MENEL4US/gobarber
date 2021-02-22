91ad72342cd43ac3d9170fcd2b5a4733e52501b9
MENEL4US <luizrodriguesbmm@gmail.com>
2021-02-22 18:59:32 -0300

    Provider controller, file controller, File model, rotas para avatar

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,26 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
-    constructor() {
-        this.init();
-    }
+  constructor() {
+    this.init();
+  }
 
-    init() {
-        this.connection = new Sequelize(databaConfig);
+  init() {
+    this.connection = new Sequelize(databaConfig);
 
-        models.map(model => model.init(this.connection));
-    }
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
+  }
 }
 
 export default new Database();
